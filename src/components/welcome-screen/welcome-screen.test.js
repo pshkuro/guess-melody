8488f78460228e408b89dfcb76dc92eceba20b73
Полина <polina110297@yandex.ru>
2020-06-13 14:39:25 +0300

    Add snapshot components test

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen";
+
+const Settings = {
+  errorsCount: 105,
+};
+
+it(`Render WelcomeScreen`, () => {
+  const tree = renderer
+  .create(
+      <WelcomeScreen
+        errorsCount={Settings.errorsCount}
+      />
+  )
+  .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
