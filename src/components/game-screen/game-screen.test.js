2fd9920eb0585bbfae0728714d5782edc70f0e67
Полина <polina110297@yandex.ru>
2020-06-23 13:06:15 +0300

    Add snapshot game-screen test

---
+++ src/components/game-screen/game-screen.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import GameScreen from "./game-screen.jsx";
+import {GameType} from "../../constants/game.js";
+
+const props = {
+  children: `<div className="children-component" />`,
+};
+
+
+describe(`GameScreen render correctly`, () => {
+
+  it(`with ArtistQuestionScreen`, () => {
+    const tree = renderer
+    .create(
+        <GameScreen
+          type={GameType.ARTIST}
+          {...props}/>
+    )
+    .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`with GenreQuestionScreen`, () => {
+    const tree = renderer
+    .create(
+        <GameScreen
+          type={GameType.GENRE}
+          {...props}/>
+    )
+    .toJSON();
+
+    expect(tree).toMatchSnapshot();
+
+  });
+});
