086917c7852976523f5695ad8af05dd043043354
Полина <polina110297@yandex.ru>
2020-06-27 14:35:49 +0300

    Install redux, react-redux. Create store and reducer and plug it.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import App from "./components/app/app.jsx";
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer.js";
 import {settings} from "./mocks/settings.js";
 import {questions} from "./mocks/questions.js";
 
+const store = createStore(reducer);
+
 
 ReactDOM.render(
-    <App
-      errorsCount={settings.errorsCount}
-      questions={questions}/>,
+    <Provider store={store}>
+      <App
+        errorsCount={settings.errorsCount}
+        questions={questions}/>,
+    </Provider>,
     document.querySelector(`#root`)
 );
