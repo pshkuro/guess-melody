cc65c0fb583e59c6d6e42287e661f7d119831533
Полина <polina110297@yandex.ru>
2020-10-03 18:18:23 +0300

    efresh commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./redux/reducer";
 
-
 const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
 
